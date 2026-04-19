5dc260947ca8e30062fd000f913f14b9206b1032
Pablo Chesini <pchesini1@gmail.com>
2026-04-19 16:21:05 +0000

    fix: corregir CORS

--- backend/src/main/java/com/gympass/gym_pass_backend/GymPassBackendApplication.java
+++ backend/src/main/java/com/gympass/gym_pass_backend/GymPassBackendApplication.java
@@ -3,10 +3,14 @@ package com.gympass.gym_pass_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GymPassBackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.ofHours(-3)));
 		SpringApplication.run(GymPassBackendApplication.class, args);
 	}
 
